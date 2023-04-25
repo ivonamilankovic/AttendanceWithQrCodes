ef696bf3611a23a6fb0da00d24a9c97e0daf2ffa
Ivona Milankovic <ivonamilankovic@gmail.com>
2023-04-25 18:28:33 +0200

    db fix - adding pk

---
+++ AttendanceWithQrCodes/Migrations/20230425161901_Pk fixes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceWithQrCodes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pkfixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "CoursesStudyProfiles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "CoursesLanguages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CoursesStudyProfiles",
+                table: "CoursesStudyProfiles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CoursesLanguages",
+                table: "CoursesLanguages",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CoursesStudyProfiles",
+                table: "CoursesStudyProfiles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CoursesLanguages",
+                table: "CoursesLanguages");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CoursesStudyProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CoursesLanguages");
+        }
+    }
+}
--- AttendanceWithQrCodes/Models/CourseStudyProfile.cs
+++ AttendanceWithQrCodes/Models/CourseStudyProfile.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AttendanceWithQrCodes.Models
 {
-    [Keyless]
     public class CourseStudyProfile
     {
+        [Key]
+        public int Id { get; set; }
+
         [ForeignKey("CourseId")]
         public int? CourseId { get; set; }
         public Course? Course { get; set; } 
