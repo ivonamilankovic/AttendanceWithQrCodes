302c39f4602cebfb765db010ac3c0d917a33b5a2
Ivona Milankovic <ivonamilankovic@gmail.com>
2023-05-18 17:31:55 +0200

    exporting attendance as excel file

---
+++ AttendanceWithQrCodes/Models/DTOs/StudentAttendanceForExcelDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AttendanceWithQrCodes.Models.DTOs
+{
+    public class StudentAttendanceForExcelDto
+    {
+        public int Index { get; set; } = default!;
+        public string Student { get; set; } = default!;
+        public bool Present { get; set; } = default!;
+        public DateTime DateOfAttendance { get; set; }
+        public string CourseName { get; set; } = default!;
+        public string LectureName { get; set; } = default!;
+        public string Lecturer { get; set; } = default!;
+    }
+}
--- AttendanceWithQrCodes/Profiles/StudentAttendanceProfile.cs
+++ AttendanceWithQrCodes/Profiles/StudentAttendanceProfile.cs
@@ -23,6 +23,26 @@ namespace AttendanceWithQrCodes.Profiles
                 .ReverseMap();
             CreateMap<StudentAttendance, StudentAttendanceListDto>()
                 .ReverseMap();
+            CreateMap<StudentAttendance, StudentAttendanceForExcelDto>()
+                .ForMember(
+                    dest => dest.Index,
+                    src => src.MapFrom(s => s.StudentIndex))
+                .ForPath(
+                    dest => dest.Student,
+                    src => src.MapFrom(s => $"{s.Student.User.FirstName} {s.Student.User.LastName}"))
+                .ForMember(
+                    dest => dest.DateOfAttendance,
+                    src => src.MapFrom(s => s.Date))
+                .ForPath(
+                    dest => dest.CourseName,
+                    src => src.MapFrom(s => s.Lecture.Course.Name))
+                .ForPath(
+                    dest => dest.LectureName,
+                    src => src.MapFrom(s => s.Lecture.Name))
+                .ForPath(
+                    dest => dest.Lecturer,
+                    src => src.MapFrom(s => $"{s.Lecture.Lecturer.FirstName} {s.Lecture.Lecturer.LastName}"))
+                .ReverseMap();
         }
     }
 }
